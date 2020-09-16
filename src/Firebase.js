6d152ac62d8e9dff7b58625b497721a7ed6bd98e
Mike Torres <apolloproductionsdj@gmail.com>
2020-09-16 13:59:02 -0700

    Initial commit

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCTxAisFCnmDjhb9VxeD7oVe4Bm3zG_6kY",
+  authDomain: "whatsapp-mern-cd4d0.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-cd4d0.firebaseio.com",
+  projectId: "whatsapp-mern-cd4d0",
+  storageBucket: "whatsapp-mern-cd4d0.appspot.com",
+  messagingSenderId: "1043542001076",
+  appId: "1:1043542001076:web:f49ae46bdbba1e63f41ee1",
+  measurementId: "G-Q7VWGJF8VY",
+};

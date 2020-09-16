fa02835b6885e69a930966bfa8c58b92d0565098
Mike Torres <apolloproductionsdj@gmail.com>
2020-09-16 14:24:12 -0700

    set up App.js to hold <Chat /> and <Sidebar />

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build a whatsapp mern clone</h1>
+    <div className="app">
+      <div className="app_body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }

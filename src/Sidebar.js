197380147aeb2e9d45c890d71349a3bb5771cede
Mike Torres <apolloproductionsdj@gmail.com>
2020-09-17 10:02:49 -0700

    added Avatar and chat info to Sidebar.js, added SidbarChat.js to hold information regarding each chat

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,52 @@
 import React from "react";
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import SidebarChat from "./SidebarChat.js";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am a Sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar
+          src="https://scontent.fphx1-2.fna.fbcdn.net/v/t1.0-1/p240x240/117926630_725007588339988_2346673801150432132_n.jpg?_nc_cat=102&_nc_sid=dbb9e7&_nc_ohc=u53pGIFFNYIAX8jBE2w&_nc_ht=scontent.fphx1-2.fna&tp=6&oh=1f0b1440903e49b56a4cc9aaccd94a24&oe=5F8ADEB8"
+          alt=""
+        />
+
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      {/* didebar Header Chat Box*/}
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
     </div>
   );
 }
